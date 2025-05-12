b5dbbab113476e2d306416e917175d1781bd2448
Giorgia Marcantonio <giorgia.marcantonio@outlook.it>
2025-05-12 17:54:34 +0200

    Scaffolding e implementazione del login e register con Fortify

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Homepage</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
