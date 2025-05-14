8c27d543b79f83dd57d66082cb360d91dca5b857
hache183 <marcoepicoco2@gmail.com>
2025-05-14 16:21:16 +0200

    aggiunta logiche modifica username

--- app/Livewire/Homepage.php
+++ app/Livewire/Homepage.php
@@ -2,11 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class Homepage extends Component
 {
+    public $username;
+    public function updateUsername() {
+        $user=User::find(Auth::id());
+        if ($user->id == Auth::id()) {
+            $user->update([
+            'name'=>$this->username
+            ]);
+            $user->save();
+            $this->reset();
+        }
+    }
+    public $showInput=false;
+    public function showInputs() {
+        $this->showInput=true;
+    }
     public function render()
     {
         $categories = Category::all();
--- resources/views/livewire/homepage.blade.php
+++ resources/views/livewire/homepage.blade.php
@@ -2,28 +2,26 @@
     <div class="container py-5">
         <div class="row justify-content-center">
             <div class="col-12 col-md-8">
-                <form class="card p-5 shadow">
-                    <div class="mb-3">
+                <form wire:submit.prevent="updateUsername">
+              <div class="mb-3">
                         <label for="name" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="name">
+                        <input type="text" class="form-control" id="name" wire:model="username">
+                        <button class="btn btn-outline-secondary" type="submit">Aggiorna</button>
                     </div>
+                </form>
+                <form class="card p-5 shadow" wire:submit.prevent="{{$showInput ? 'saveInputs' : 'showInputs'}}">
                     <div class="mb-3">
                         <label for="description" class="form-label">Description</label>
                         <input type="text" class="form-control" id="description">
                     </div>
-                        <div class="input-group">
-                            <select class="selectpicker" data-live-search="true" id="category" aria-label="Default select example">
-                                <option selected disabled>Open this select menu</option>
-                                @foreach ($categories as $category)
-                                <option data-content="<i class='bi bi-{{$category->icons}}'></i>">
-                                
-                                    {{$category->name}}
-                               
-                                </option>
-                                @endforeach
-                                
-                            </select>
-                        </div>
+                    @if($showInput)
+                    @foreach ($categories as $category)
+                    <button class="btn btn-outline-secondary">
+                        <i class="bi bi-{{$category->icons}}">
+                        </i>
+                    </button>
+                    @endforeach
+                    @endif
                     <div class="mb-3 d-flex justify-content-center flex-column aling-items-center">
                         <button type="submit" class="button-64"><span class="text">Aggiungi Link</span></button>
                     </div>
